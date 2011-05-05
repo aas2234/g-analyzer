b7253d864f16002662d15f59b0c9ea8a357ead65
Abhishek Srivastava <aas2234@columbia.edu>
2011-05-05 18:13:19 -0400

    tester for DD with EdgeList graph

--- src/edu/columbia/gAnalyzer/worker/ClusteringCoeffWorker.java
+++ src/edu/columbia/gAnalyzer/worker/ClusteringCoeffWorker.java
@@ -2,6 +2,7 @@ package edu.columbia.gAnalyzer.worker;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -14,7 +15,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class ClusteringCoeffWorker extends MRGWorker {
 	
 	
-	public static class ALCLusteringCoeffMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class ALCLusteringCoeffMapper1 extends Mapper<LongWritable, Text, Text, IntWritable> {
 		private static final IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 		
@@ -25,7 +26,7 @@ public class ClusteringCoeffWorker extends MRGWorker {
 		}
 	}
 	
-	public static class ALClusteringCoeffReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class ALClusteringCoeffReducer1 extends Reducer<Text, IntWritable, Text, IntWritable> {
 		
 		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 	    
@@ -33,7 +34,7 @@ public class ClusteringCoeffWorker extends MRGWorker {
 	}
 
 	
-	public static class ELCLusteringCoeffMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class ALCLusteringCoeffMapper2 extends Mapper<LongWritable, Text, Text, IntWritable> {
 		private static final IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 		
@@ -44,7 +45,50 @@ public class ClusteringCoeffWorker extends MRGWorker {
 		}
 	}
 	
-	public static class ELClusteringCoeffReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class ALClusteringCoeffReducer2 extends Reducer<Text, IntWritable, Text, IntWritable> {
+		
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+	    
+		}
+	}
+	
+/*********************************************************************************************************************************/
+	
+	public static class ELCLusteringCoeffMapper1 extends Mapper<LongWritable, Text, LongWritable, LongWritable> {
+		private static final IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			LongWritable nodeID = new LongWritable(Long.parseLong(tokenizer.nextToken()));
+			LongWritable neighbor = new LongWritable(Long.parseLong(tokenizer.nextToken()));
+			
+			context.write(nodeID,neighbor);
+		}
+	}
+	
+	public static class ELClusteringCoeffReducer1 extends Reducer<Text, IntWritable, Text, IntWritable> {
+		
+		public void reduce(LongWritable key, Iterator<IntWritable> values, OutputCollector<LongWritable, IntWritable> output, Reporter reporter) throws IOException {
+		    
+			
+		}
+	}
+	
+	
+	public static class ELCLusteringCoeffMapper2 extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private static final IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		
+		public void map(LongWritable key, Text value, Context context) throws IOException {
+		    String line = value.toString();
+		    
+			
+		}
+	}
+	
+	public static class ELClusteringCoeffReducer2 extends Reducer<Text, IntWritable, Text, IntWritable> {
 		
 		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
 	    
